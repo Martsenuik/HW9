56d2c3d48efdf7e148eacca31b5e0ed2240136c3
Martseniuk Alina <martseniuk.av@gmail.com>
2024-12-04 22:06:26 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+//*\\\\\\\\\\\\Завдання 1\\\\\\\\\\\\\\\
+
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+      
+//     console.log(`${i + 1} - ${array[i]}`);
+//   }
+// };
+
+// const arr = ['Mango', 'Poly', 'Ajax'];
+// logItems(arr);
+
+//?\\\\\\\\\\\\Завдання 2\\\\\\\\\\\\\\\
+
+function calculateEngravingPrice(message, pricePerWord) {
+
+}
+//?\\\\\\\\\\\\Завдання 3\\\\\\\\\\\\\\\
+
+function findLongestWord(string) {
+    
+}
+
+//?\\\\\\\\\\\\Завдання 4\\\\\\\\\\\\\\\
+
+function formatString(string) {
+    
+}
+
+//?\\\\\\\\\\\\Завдання 5\\\\\\\\\\\\\\\
+
+function checkForSpam(message) {
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
